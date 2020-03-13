367653e3c0fabe53229fa1ac829d86cc740e3319
dungqa98@gmail.com <tiendung98>
2020-03-13 22:45:07 +0700

    tao nav, bo cuc phia duoi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongComponent } from './components/song/song.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SongComponent,
   ],
   imports: [
     BrowserModule,
