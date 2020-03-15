5cf9b79de6b6012d72c712c7eaf6c7f85cbe25ee
dungqa98@gmail.com <tiendung98>
2020-03-15 18:29:39 +0700

    play music

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SongComponent } from './components/song/song.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SongComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/modules/user.module.ts
+++ src/app/modules/user.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {LiveComponent} from '../components/live/live.component';
 import {SongService} from '../services/song.service';
+import {SongComponent} from '../components/song/song.component';
 
 const routes: Routes = [
   {
     path: '', component: LiveComponent
+  },
+  {
+    path: 'song', component: SongComponent
   }
 ];
 
 @NgModule({
-  declarations: [LiveComponent],
+  declarations: [LiveComponent, SongComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
