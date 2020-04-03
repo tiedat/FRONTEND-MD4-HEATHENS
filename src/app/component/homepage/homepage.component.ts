d10288cd075ce80da26f0baa76b3923b3b640f49
dungqa98@gmail.com <tiendung98>
2020-04-04 00:14:10 +0700

    Done Edit, edit delete

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SongService} from '../../services/song.service';
+import {UserService} from '../../services/user.service';
+import {DataService} from '../../services/data.service';
 
 @Component({
   selector: 'app-homepage',
@@ -8,11 +10,19 @@ import {SongService} from '../../services/song.service';
 })
 export class HomepageComponent implements OnInit {
   songList: any[];
-  constructor(private userService: SongService) { }
+  username: string;
+  constructor(private songService: SongService,
+              private userService: UserService,
+              private data: DataService ) { }
 
   ngOnInit() {
-    this.userService.getAllSong().subscribe( result => {
-      this.userService = result.data;
+    this.data.currentMessage.subscribe(username => this.username = username);
+    this.userService.getUserByUsername(this.username).subscribe(user => {
+      console.log(user);
+    });
+    this.songService.getAllSong().subscribe( result => {
+      this.songList = result.data;
+      console.log(this.songList);
     });
   }
 
