83b31dab50f5a4ed6924009cbdf01fe5a3ecd316
dungqa98@gmail.com <tiendung98>
2020-04-20 23:39:19 +0700

    show all my playlist

--- src/app/personal-music/my-all-playlist/my-all-playlist.component.ts
+++ src/app/personal-music/my-all-playlist/my-all-playlist.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {IPlaylist} from '../../interface/playlist';
+import {DataService} from '../../services/data.service';
+import {PlaylistService} from '../../services/playlist.service';
+import {FormBuilder, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-my-all-playlist',
@@ -6,10 +11,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-all-playlist.component.scss']
 })
 export class MyAllPlaylistComponent implements OnInit {
+  imgDetail: any;
+  message: string;
+  isShow = false;
+  isSuccess = true;
+  isLoading = false;
   listPlayList: any[];
-  constructor() { }
-
-  ngOnInit(): void {
+  playlistForm: any;
+  playlist: IPlaylist = {
+    name: '',
+    descriptionPlaylist: '',
+    songs: [],
+    user: {},
+  };
+  username: any;
+  constructor(private data: DataService,
+              private playlistService: PlaylistService,
+              private fb: FormBuilder,
+              private userService: UserService) { }
+  ngOnInit() {
+    this.data.currentMessage.subscribe(username => this.username = username);
+    this.userService.getUserByUsername(this.username).subscribe(user => {
+      console.log(user);
+      this.playlist.user = user.data;
+    });
+    this.playlistService.getAllPlaylistByUser(this.username).subscribe(list => {
+      console.log(list);
+      this.listPlayList = list.data;
+    });
+    this.playlistForm = this.fb.group({
+      name: this.fb.control('', [Validators.required]),
+      descriptionPlaylist: '',
+    });
+    this.data.currentMessage.subscribe(username => {
+      console.log(username);
+      this.username = username;
+    });
   }
-
+  showPlayList() {
+  }
+  creatPlaylist() {
+    this.playlist.name = this.playlistForm.get('name').value;
+    console.log(this.playlist);
+    this.playlistService.createPlaylist(this.playlist).subscribe( result => {
+      this.isShow = true;
+      this.isSuccess = true;
+      this.message = 'Tạo thành công!';
+      this.isLoading = false;
+      this.ngOnInit();
+    }, error => {
+      this.isShow = true;
+      this.isSuccess = false;
+      this.message = 'Tạo thất bại!';
+      this.isLoading = false;
+    });
+  }
 }
