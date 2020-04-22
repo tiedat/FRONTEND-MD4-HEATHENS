19e5a59a61f1357d77f99d06ed1a372f870bcf50
dungqa98@gmail.com <tiendung98>
2020-04-22 11:11:13 +0700

    Revert "Bước khởi đầu, hoàn thành search playlist"
    This reverts commit 48d70242

---
+++ src/app/personal-music/my-all-playlist/my-all-playlist.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import {IPlaylist} from '../../interface/playlist';
+import {DataService} from '../../services/data.service';
+import {PlaylistService} from '../../services/playlist.service';
+import {FormBuilder, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+
+@Component({
+  selector: 'app-my-all-playlist',
+  templateUrl: './my-all-playlist.component.html',
+  styleUrls: ['./my-all-playlist.component.scss']
+})
+export class MyAllPlaylistComponent implements OnInit {
+  imgDetail: any;
+  message: string;
+  isShow = false;
+  isSuccess = true;
+  isLoading = false;
+  listPlayList: any[];
+  playlistForm: any;
+  playlist: IPlaylist = {
+    name: '',
+    img: '',
+    descriptionPlaylist: '',
+    songs: [],
+    user: {},
+  };
+  username: any;
+  constructor(private data: DataService,
+              private playlistService: PlaylistService,
+              private fb: FormBuilder,
+              private userService: UserService) { }
+  ngOnInit() {
+    this.data.currentMessage.subscribe(username => this.username = username);
+    this.userService.getUserByUsername(this.username).subscribe(user => {
+      console.log(user);
+      this.playlist.user = user.data;
+    });
+    this.playlistService.getAllPlaylistByUser(this.username).subscribe(list => {
+      console.log(list);
+      this.listPlayList = list.data;
+    });
+    this.playlistForm = this.fb.group({
+      name: this.fb.control('', [Validators.required]),
+      descriptionPlaylist: '',
+    });
+    this.data.currentMessage.subscribe(username => {
+      console.log(username);
+      this.username = username;
+    });
+  }
+  showPlayList() {
+  }
+  creatPlaylist() {
+    this.playlist.name = this.playlistForm.get('name').value;
+    this.playlist.img = 'https://photo-zmp3.zadn.vn/album_default.png';
+    console.log(this.playlist);
+    this.playlistService.createPlaylist(this.playlist).subscribe( result => {
+      this.isShow = true;
+      this.isSuccess = true;
+      this.message = 'Tạo thành công!';
+      this.isLoading = false;
+      this.ngOnInit();
+    }, error => {
+      this.isShow = true;
+      this.isSuccess = false;
+      this.message = 'Tạo thất bại!';
+      this.isLoading = false;
+    });
+  }
+}
