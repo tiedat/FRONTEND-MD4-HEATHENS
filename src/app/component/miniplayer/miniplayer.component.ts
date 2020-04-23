e38db3019132823cb6de52450830b252a672b2fb
lethanhhai1998 <49983494+lethanhhai1998@users.noreply.github.com>
2020-04-23 12:06:56 +0700

    update

--- src/app/component/miniplayer/miniplayer.component.ts
+++ src/app/component/miniplayer/miniplayer.component.ts
@@ -13,13 +13,25 @@ import { ISong } from 'src/app/interface/song';
 export class MiniplayerComponent implements OnInit {
   songList: Observable<ISong[]>;
   currentSong: ISong;
-  songIndex = 0
+  shufferList: ISong[];
+  songIndex = 0;
+  length;
+  isRepeat = false;
+  isShuffe = false;
   constructor(private songService: SongService,
     private playerService: PlayerService) { }
 
   ngOnInit(): void {
     this.songList = this.playerService.player$;
-    this.songList.subscribe(song => this.currentSong = song[this.songIndex]);
+
+    this.songList.subscribe(song => {
+      console.log(song);
+      this.currentSong = song[this.songIndex];
+      this.length = song.length;
+      //this.shufferList = this.shuffle(song);
+
+    });
+    console.log(this.shufferList);
   }
   upNumberOfPlays() {
     this.currentSong.numberOfPlays = this.currentSong.numberOfPlays + 1;
@@ -27,8 +39,38 @@ export class MiniplayerComponent implements OnInit {
   }
   nextSong() {
     this.upNumberOfPlays();
-    this.songIndex++;
-    this.songList.subscribe(song => this.currentSong = song[this.songIndex]);
+    console.log(this.songIndex);
+    console.log(this.length);
 
+    if (this.songIndex < this.length - 1) {
+      this.songIndex++;
+      console.log(this.songIndex);
+      this.songList.subscribe(song => this.currentSong = song[this.songIndex]);
+      //this.currentSong = this.shufferList[this.songIndex];
+    } else {
+      if (this.isRepeat) {
+        this.songIndex = 0;
+        this.songList.subscribe(song => this.currentSong = song[this.songIndex]);
+      }
+    }
+
+  }
+
+  backSong() {
+    if (this.songIndex > 0) {
+      this.songIndex--;
+      this.songList.subscribe(song => this.currentSong = song[this.songIndex]);
+    }
+  }
+
+  shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
   }
 }
